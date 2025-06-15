79444577bd91b78ee5f64dcf5c35bb5ab9cf495c
indiphilegogela <156888615+indiphilegogela@users.noreply.github.com>
2025-06-15 15:16:57 +0200

    update

---
+++ src/Pages/Host/HostVanPhotos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+
+export default function HostVanPhotos() {
+  const { currentVan } = useOutletContext();
+  return <img src={currentVan.imageURL} className="host-van-detail-image" />;
+}
